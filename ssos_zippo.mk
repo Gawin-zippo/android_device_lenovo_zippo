c439f2fbaf655a2e706fe5b078515c857177b80b
Gawin-zippo <knoxme.zippo@gmail.com>
2021-05-31 03:38:06 +0700

    zippo: build SSOS

--- ssos_zippo.mk
+++ ssos_zippo.mk
@@ -7,14 +7,23 @@
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+
+# Custom stuff
+SSOS_BUILD_TYPE := UNOFFICIAL
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z6 Pro
-PRODUCT_NAME := lineage_zippo
+PRODUCT_NAME := ssos_zippo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
